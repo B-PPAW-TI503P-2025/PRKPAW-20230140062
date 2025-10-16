91e0460eeb1986be597d72ca40ab775418860147
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-16 21:23:28 +0900

    Memodifikasi server.js pada folder node server

--- 20230140062-node server/server.js
+++ 20230140062-node server/server.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Server!' });
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+ const express = require('express');
+ const cors = require('cors'); 
+ const app = express();
+ const PORT = 3001;
+ 
+ // Middleware
+ app.use(cors()); 
+ app.use(express.json()); 
+ app.use((req, res, next) => {
+   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+   next();
+ });
+ 
+ app.get('/', (req, res) => {
+   res.send('Home Page for API');
+ });
+ 
+ app.listen(PORT, () => {
+   console.log(`Express server running at http://localhost:${PORT}/`);
+ });
