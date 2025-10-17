d161846a0006c3669ae7e7aeaf22dda864739d25
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-17 11:09:56 +0900

    Menambahkan beberapa file + kodingan untuk keperluan praktikum 3

--- 20230140062-node server/server.js
+++ 20230140062-node server/server.js
@@ -1,26 +1,31 @@
- const express = require('express');
- const cors = require('cors'); 
- const app = express();
- const PORT = 3001;
- const bookRoutes = require('./routes/books');
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+const morgan = require("morgan");
+const ruteBuku = require('./routes/books');
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
  
  // Middleware
- app.use(cors()); 
- app.use(express.json()); 
- app.use((req, res, next) => {
-   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-   next();
- });
- 
- app.get('/', (req, res) => {
-   res.send('Home Page for API');
- });
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+app.get("/", (req, res) => {
+  res.send("Home Page for API");
+});
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
  
  app.listen(PORT, () => {
    console.log(`Express server running at http://localhost:${PORT}/`);
  });
-
-app.use('/api/books', bookRoutes);
 
  app.use((req, res, next) => {
     res.status(404).send("Error 404: Endpoint Tidak Ditemukan.");
