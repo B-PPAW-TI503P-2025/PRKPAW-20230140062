53826b498caa01f4fbe5480b5b76f19a333ba9b3
MzSuwawa <yudharamadhan838@gmail.com>
2025-10-31 11:13:08 +0900

    Menambahkan method PUT pada Presensi

--- 20230140062-node server/controllers/presensiController.js
+++ 20230140062-node server/controllers/presensiController.js
@@ -107,3 +107,26 @@ exports.deletePresensi = async (req, res) => {
         res.status(500).json({ message: "Terjadi kesalahan pada server", error: error.message });
     }
 };
+
+exports.updatePresensi = async (req, res) => {
+  try {
+    const presensiId = req.params.id;
+    const { checkIn, checkOut } = req.body;
+    if (checkIn === undefined && checkOut === undefined && nama === undefined) {
+      return res.status(400).json({ message: "Tidak ada data yang diberikan untuk diperbarui (checkIn, checkOut, atau nama)" });
+  }
+  const recordToUpdate = await Presensi.findByPk(presensiId);
+    if (!recordToUpdate) {
+      return res.status(404).json({ message: "Catatan presensi tidak ditemukan" });
+    }
+
+    recordToUpdate.checkIn = checkIn || recordToUpdate.checkIn;
+    recordToUpdate.checkOut = checkOut || recordToUpdate.checkOut;
+    recordToUpdate.nama = nama || recordToUpdate.nama;
+    await recordToUpdate.save();
+
+    res.json({ message: "Catatan presensi berhasil diperbarui", data: recordToUpdate });
+  } catch (error) {
+  res.status(500).json({ message: "Terjadi kesalahan pada server", error: error.message });
+  }
+};
