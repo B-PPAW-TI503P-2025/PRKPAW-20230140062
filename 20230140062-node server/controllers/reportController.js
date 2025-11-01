42d3744db6fb327eb9cc6eac851b0a21292063ba
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-02 02:44:43 +0900

    Menambahkan suatu fitur agar bisa memfilter berdasarkan rentang tanggal

--- 20230140062-node server/controllers/reportController.js
+++ 20230140062-node server/controllers/reportController.js
@@ -5,8 +5,7 @@ const timeZone = "Asia/Jakarta";
 
 exports.getDailyReport = async (req, res) => {
     try {
-        const today = new Date();
-        const { nama } = req.query;
+        const { nama, tanggalMulai, tanggalSelesai } = req.query;
         let options = { where: {} };
 
         if (nama) {
@@ -15,11 +14,33 @@ exports.getDailyReport = async (req, res) => {
             };
         }
 
+        if (tanggalMulai && tanggalSelesai) {
+            const endDate = new Date(tanggalSelesai);
+            endDate.setHours(endDate.getDate() + 1);
+
+            options.where.checkIn = {
+                [Op.between]: [
+                    new Date(tanggalMulai),
+                    endDate
+                ]
+            };
+        }
+
         const records = await Presensi.findAll(options);
 
-        res.json({
-            reportDate: new Date().toLocaleString(), data: records,
+        const formattedRecords = records.map(record => ({
+            userId: record.userId,
+            nama: record.nama,
+            checkIn: format(record.checkIn, 'yyyy-MM-dd HH:mm:ss', { timeZone }),
+            checkOut: record.checkOut ? format(record.checkOut, 'yyyy-MM-dd HH:mm:ss', { timeZone }) : 'N/A'
+        }));
+
+        res.status(200).json({
+            message: "Laporan presensi berhasil diambil",
+            count: formattedRecords.length,
+            data: formattedRecords,
         });
+
     } catch (error) {
         res.status(500).json({ message: "Terjadi kesalahan pada server", error: error.message });
     }
