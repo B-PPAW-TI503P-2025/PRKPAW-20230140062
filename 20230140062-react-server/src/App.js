f5e4a136759264207b0c55d0b6696d9f4a443a2b
MzSuwawa <yudharamadhan838@gmail.com>
2025-11-14 11:18:33 +0900

    Melengkapi kode untuk keperluan praktikum pertemuan 7

--- 20230140062-react-server/src/App.js
+++ 20230140062-react-server/src/App.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import DashboardPage from './components/DashboardPage';
 
 function App() {
-  const [name, setName] = useState(''); 
-
-  const handleNameChange = (event) => {
-    setName(event.target.value); 
-  };
-
   return (
-    <div>
-      <h1>Hello, {name || '[nama]'}!</h1> 
-
-      <p>Masukkan nama Anda:</p>
-      <input
-        type="text"
-        value={name}
-        onChange={handleNameChange}
-        placeholder="Ketik nama di sini"
-      />
-    </div>
+    <Router>
+      <div>
+        {/* Navigasi ini bisa dihapus jika tidak diperlukan */}
+        <nav className="p-4 bg-gray-100">
+          <Link to="/login" className="mr-4">Login</Link>
+          <Link to="/register">Register</Link>
+        </nav>
+        
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/" element={<LoginPage />} /> 
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
 export default App;
